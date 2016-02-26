c4af8faac279c50ee10b548460fa4fb5ff11ace9
RoyTimes <roytimes@icloud.com>
2016-02-26 17:27:18 +0800

    Opensource this code

---
+++ README.md
@@ -0,0 +1,14 @@
+## md2HTML
+
+A simple JavaScript script for batch conversion from .md to .html
+using [Remarkable](https://github.com/jonschlinkert/remarkable)
+
+## Install
+1. Clone this repo `git clone https://github.com/RoyTimes/md2html.git`
+2. build this app `npm install`
+3. Place the markdowns you want to convert @ /markdown folder
+4. run `npm start`
+5. HTML files would be generated @ /html folder
+
+## Author 
+Song Zhou
---
+++ index.js
@@ -0,0 +1,47 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Song Zhou
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+var fs = require('fs');
+var path = require('path');
+var Remarkable = require('remarkable');
+var md = new Remarkable('full');
+
+function render(dir) {
+    var files = fs.readdirSync(dir);
+    for (file in files) {
+        var file_name = files[file];
+
+        if (file_name.substr(file_name.length - 3) === ".md") {
+            console.log("File: " + file_name + " found!");            
+            var txt = fs.readFileSync(__dirname + "/markdown/" + file_name);
+
+            fs.writeFileSync(
+                './html/' + file_name.substr(0,file_name.length - 3) + ".html",
+                md.render(txt.toString()));
+            console.log("File: " + 
+                    file_name.substr(0,file_name.length - 3) + 
+                    ".html" + " Write Success!")
+        }
+    }
+}
+
+render('./markdown/');
